00b2ae74cd9445ae6a854fd803942226ddd061e3
Anıl Aydın <anil_aydinn@outlook.com>
2019-07-24 02:24:59 +0300

    New Commit

---
+++ CharBoolean/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Char Veri Tipi : Karakter göstermek için kullanılır. 2 byte yer kaplar. 2 ^ 16 tane karakter simgelenebilir.
+		//Boolean Veri Tipi : Koşul Durumlarında kullanılır. true veya false değeri alır.
+		
+		char a = 'A';
+		char b = '?';
+		char c = 2000;
+		char d = '\u0152';
+		char e = '\u063C';
+		boolean y = true;
+		boolean x = false;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(y);
+		System.out.println(x);
+		
+	}
+}
