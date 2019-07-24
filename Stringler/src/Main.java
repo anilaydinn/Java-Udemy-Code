467258ac69e1293d972cac50465b551da731f2db
Anıl Aydın <anil_aydinn@outlook.com>
2019-07-24 03:02:51 +0300

    New Commit

---
+++ Stringler/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = "Java\tProgramlama\tDili";
+		String b = "Java\nProgramlama\nDili";
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
