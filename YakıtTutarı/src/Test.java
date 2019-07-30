295ca8839b58d98e1d117ca20ed3c12b8064f912
Anıl Aydın <anil_aydinn@outlook.com>
2019-07-30 17:45:00 +0300

    New Commit

---
+++ YakıtTutarı/src/Test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		/*
+		 	Bir aracın kilometrede ne kadar yaktığı ve kaç kilometre yol yaptığı bilgilerini alın ve sürücünün toplam ne kadar ödemesi gerektiğini hesaplayın.
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Aracınız kilometrede kaç kuruş yakıyor? (Örnek : 0,32): ");
+		double kurus = scanner.nextDouble();
+		System.out.print("Aracınızla kaç kilometre gittiniz? : ");
+		int km = scanner.nextInt();
+		
+		System.out.println("Toplam ödemeniz gereken tutar : " + (kurus * km) + " tl dir...");
+	}
+}
