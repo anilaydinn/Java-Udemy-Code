71b16e14b710daf348efb9447c90f529966117e6
Anıl Aydın <anil_aydinn@outlook.com>
2019-07-30 17:06:57 +0300

    New Commit

---
+++ InputAlma/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Lütfen Yaşınızı Girin:");
+		
+		int yas = scanner.nextInt();
+		
+		System.out.println("Yaşınız: " + yas);
+	}
+}
