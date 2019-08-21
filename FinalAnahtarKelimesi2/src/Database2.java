7c89a739c80611b97c0d0b0f8f460ef2f556ff58
Anıl Aydın <anil_aydinn@outlook.com>
2019-08-21 18:32:45 +0300

    New Commit

---
+++ FinalAnahtarKelimesi2/src/Database2.java
@@ -0,0 +1,8 @@
+
+public class Database2 extends Database {
+
+	public void baglantiKur(String username, String password) {
+		
+		
+	}
+}
---
+++ FinalAnahtarKelimesi2/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Database database = new Database();
+		database.baglanti_kur("root", "12345");
+	}
+}
