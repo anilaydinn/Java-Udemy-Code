21b7aa0b9191fe31a6a53a17119f0e96c821408f
Anıl Aydın <anil_aydinn@outlook.com>
2019-08-21 15:23:32 +0300

    New Commit

---
+++ Paketler1/src/com/anilaydin/deneme/Main.java
@@ -0,0 +1,12 @@
+package com.anilaydin.deneme;
+
+import com.anilaydin.swingornegi.Pencere;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Pencere pencere = new Pencere();
+		
+	}
+}
