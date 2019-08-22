58ad521d70e23e674bc1ff1e8cf6d39b337a5abc
Anıl Aydın <anil_aydinn@outlook.com>
2019-08-22 13:59:01 +0300

    New Commit

---
+++ InterfacelerDetay/src/IDeneme.java
@@ -0,0 +1,16 @@
+
+public interface IDeneme {
+
+	public int a = 4;
+	
+	public int b = 5;
+	
+	public static String deneme = "Deneme";
+	
+	public final int c = 6;
+	
+	public static void deneme() {
+		
+		System.out.println("Deneme");
+	}
+}
